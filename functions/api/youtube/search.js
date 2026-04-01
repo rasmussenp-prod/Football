d171982b8fc4a3c5ba5ac79365b9c381c3898b6f
paulrasmussen <rasmussenp@gmail.com>
2026-04-01 19:27:23 +0200

    Fixing Google search api

--- functions/api/youtube/search.js
+++ functions/api/youtube/search.js
@@ -1,12 +1,31 @@
 export async function onRequestGet(context) {
-  const { env } = context;
+  const { request, env } = context;
+  const url = new URL(request.url);
 
-  return new Response(JSON.stringify({
-    hasKey: !!env.YOUTUBE_API_KEY,
-    keyPreview: env.YOUTUBE_API_KEY
-      ? env.YOUTUBE_API_KEY.slice(0, 6)
-      : null
-  }), {
-    headers: { "content-type": "application/json" }
+  const q = url.searchParams.get("q");
+  if (!q) {
+    return json({ error: "Missing q" }, 400);
+  }
+
+  const upstream = new URL("https://www.googleapis.com/youtube/v3/search");
+  upstream.searchParams.set("part", "snippet");
+  upstream.searchParams.set("type", "video");
+  upstream.searchParams.set("maxResults", "3");
+  upstream.searchParams.set("q", q);
+  upstream.searchParams.set("key", env.YOUTUBE_API_KEY);
+
+  const res = await fetch(upstream.toString());
+  const data = await res.json();
+
+  return json(data, res.status);
+}
+
+function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      "content-type": "application/json; charset=utf-8",
+      "cache-control": "no-store"
+    }
   });
-} 
+}
