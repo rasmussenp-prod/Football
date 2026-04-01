fb3748b618748e2d67701d72914dc691e9650a38
paulrasmussen <rasmussenp@gmail.com>
2026-04-01 19:14:39 +0200

    APIs still

--- functions/api/youtube/search.js
+++ functions/api/youtube/search.js
@@ -1,35 +1,12 @@
 export async function onRequestGet(context) {
-  const { request, env } = context;
-  const url = new URL(request.url);
+  const { env } = context;
 
-  const q = url.searchParams.get("q");
-  if (!q) {
-    return json({ error: "Missing q" }, 400);
-  }
-
-  const upstream = new URL("https://www.googleapis.com/youtube/v3/search");
-  upstream.searchParams.set("part", "snippet");
-  upstream.searchParams.set("type", "video");
-  upstream.searchParams.set("maxResults", "3");
-  upstream.searchParams.set("order", "relevance");
-  upstream.searchParams.set("q", q);
-  upstream.searchParams.set("regionCode", "GB");
-  upstream.searchParams.set("relevanceLanguage", "en");
-  upstream.searchParams.set("safeSearch", "strict");
-  upstream.searchParams.set("key", env.YOUTUBE_API_KEY);
-
-  const res = await fetch(upstream.toString());
-  const data = await res.json();
-
-  return json(data, res.status, 21600);
-}
-
-function json(data, status = 200, cacheSeconds = 0) {
-  return new Response(JSON.stringify(data), {
-    status,
-    headers: {
-      "content-type": "application/json; charset=utf-8",
-      "cache-control": `public, max-age=${cacheSeconds}`
-    }
+  return new Response(JSON.stringify({
+    hasKey: !!env.YOUTUBE_API_KEY,
+    keyPreview: env.YOUTUBE_API_KEY
+      ? env.YOUTUBE_API_KEY.slice(0, 6)
+      : null 
+  }), {
+    headers: { "content-type": "application/json" }
   });
 }
